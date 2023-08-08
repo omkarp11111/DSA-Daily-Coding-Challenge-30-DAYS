afeef6dd9ce12949ab328fb87ce2d6c10bc2016d
Omkar Papade <127607966+omkarp11111@users.noreply.github.com>
2023-08-09 01:05:45 +0530

    return true if strings are anagrams
    How do you check if two strings are anagrams of each other?

---
+++ check_anagrams_string.cpp
@@ -0,0 +1,31 @@
+// How do you check if two strings are anagrams of each other?
+#include <iostream>
+#include <algorithm>
+using namespace std;
+bool CheckAnagrams(string str1, string str2)
+{
+
+  if (str1.length() != str2.length())
+    return false;
+ 
+  sort(str1.begin(), str1.end());
+  sort(str2.begin(), str2.end());
+ 
+ 
+  for (int i = 0; i < str1.length(); i++)
+  {
+    if (str1[i] != str2[i])
+      return false;
+  }
+  return true;
+}
+int main()
+{
+  string Str1 = "INTEGER";
+  string Str2 = "TEGERNI";
+  if(CheckAnagrams(Str1, Str2))
+  cout << "True" << endl;
+  else
+  cout<<"False"<<endl;
+  return 0;
+}
